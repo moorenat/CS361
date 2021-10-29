7f89d61876586930b8e7d40743a5acdf91d090f4
Nate Moore <natemoore2088@gmail.com>
2021-10-29 13:53:58 -0600

    sounds working

---
+++ index.js
@@ -0,0 +1,39 @@
+// sources cited: Used this guide to get handlebars/express/node running locally:
+//https://waelyasmina.medium.com/a-guide-into-using-handlebars-with-your-express-js-application-22b944443b65
+
+const router = module.exports = require('express').Router();
+const axios = require('axios').default;
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+//Loads the handlebars module
+const handlebars = require('express-handlebars');
+//Sets our app to use the handlebars engine
+app.set('view engine', 'handlebars');
+//Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+}));
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('main', { layout: 'index' });
+});
+
+app.use('/results', require('./results.js'));
+
+
+app.use(function (req, res) {
+    res.status(404);
+    res.render('404', { layout: 'index' });
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500', { layout: 'index' });
+});
+
+app.listen(port, () => console.log(`App listening to port ${port}`));
---
+++ results.js
@@ -0,0 +1,50 @@
+
+const axios = require('axios').default;
+const express = require('express');
+const router = module.exports = express.Router();
+const apikey = '6DTu2UoSYHSrVkZidKBlE3BAjac4Nh95KbMpJ5bA'
+const soundurl = 'https://freesound.org/apiv2/search/text/?query='
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json()) // To parse the incoming requests with JSON payloads
+
+async function sound_search(url) {
+
+    let response = await axios.get(url);
+    let data = JSON.stringify(response.data)
+    return JSON.parse(data)
+    // console.log(data)
+
+};
+
+router.post('/', function (req, res) {
+    console.log(req.body)
+    let keys = req.body.keys
+    let context = sound_search(soundurl + keys + '&fields=name,description,previews' + '&token=' + apikey)
+        .then((context) => {
+            res.status(200);
+            res.render('results', {
+                layout: 'index',
+                context
+            });
+            console.log(context)
+        })
+})
+
+router.get('/:keys', function (req, res) {
+    console.log(req.params)
+    let keys = req.params.keys
+    let context = sound_search(soundurl + keys + '&fields=name,description,previews' + '&token=' + apikey)
+        .then((context) => {
+            res.status(200);
+            res.render('results', {
+                layout: 'index',
+                context
+            });
+            console.log(context)
+            return context
+
+        })
+});
+
+
+module.exports = router;
