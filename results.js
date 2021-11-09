1aef70289c78431cdc7fd64c922a27ac05dfc2db
Nate Moore <natemoore2088@gmail.com>
2021-11-09 14:37:13 -0700

    Revert "da"
    This reverts commit b9f7dda06c0db86f860899318246952146790183.

--- results.js
+++ results.js
@@ -1,4 +1,4 @@
-const url = require('url')
+
 const axios = require('axios').default;
 const express = require('express');
 const router = module.exports = express.Router();
@@ -6,88 +6,52 @@ const apikey = '6DTu2UoSYHSrVkZidKBlE3BAjac4Nh95KbMpJ5bA'
 const soundurl = 'https://freesound.org/apiv2/search/text/?query='
 router.use(express.urlencoded({ extended: true }));
 router.use(express.json()) // To parse the incoming requests with JSON payloads
-const { Datastore } = require('@google-cloud/datastore')
-const STATE = "State"
-const datastore = new Datastore();
 
+async function sound_search(url) {
 
-function fromDatastore(item) {
-    item.id = item[Datastore.KEY].id;
-    return item;
-}
-function get_states() {
-    const q = datastore.createQuery(STATE);
-    return datastore.runQuery(q).then((entities) => {
-        return entities[0].map(fromDatastore)
-    });
-}
-// async function sound_search(url) {
+    let response = await axios.get(url);
+    let data = JSON.stringify(response.data)
+    return JSON.parse(data)
+    // console.log(data)
 
-//     let response = await axios.get(url);
-//     let data = JSON.stringify(response.data)
-//     return JSON.parse(data)
-//     // console.log(data)
+};
 
-// };
-
-const client_id = '897198022231-dkfkrdbhftau0d4dm7pt9ipo3o8r1knq.apps.googleusercontent.com'
-// const redirect_uri = 'https://moorenat-oauth.wl.r.appspot.com/results'
-const redirect_uri = 'http://localhost:8080/results'
-const client_secret = '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'
-let token = []
-router.get('/', function (req, res) {
-    query = req.query
-    state = query.state
-    code = query.code
-    console.log(state)
-    let check = false
-    let states = get_states().then((states) => {
-        for (i = 0; i < states.length; i++) {
-            curState = states[i]
-            if (curState.state === state) {
-                check = true
-                break
-            }
-        }
-    }).then((output) => {
-        return check
-    })
-    states.then(function (result) {
-        if (result) {
-            axios.post('https://www.googleapis.com/oauth2/v4/token', {
-                'code': code,
-                'client_id': client_id,
-                'client_secret': client_secret,
-                'redirect_uri': redirect_uri,
-                'grant_type': 'authorization_code'
-
-            }).then(function (response) {
-                // console.log(response.data)
-                data = response.data
-                token.push(data)
-                return data
-            }).catch(function (error) {
-                // console.log(error)
-            }).then(function (response) {
-                // console.log(data)
-
-                return data
-            })
-        }
-        else {
-            res.status(400).send('Oops')
-        }
-    })
-    console.log(token)
-    res.render('results', {
-        layout: 'index'
-        // context
-    })
-
-
-
+router.post('/', function (req, res) {
+    console.log(req.body)
+    let keys = req.body.keys
+    let context = sound_search(soundurl + keys + '&fields=name,description,previews' + '&token=' + apikey)
+        .then((context) => {
+            res.status(200);
+            res.render('results', {
+                layout: 'index',
+                context
+            });
+            console.log(context)
+        })
 })
 
+router.get('/:keys', function (req, res) {
+    console.log(req.params)
+    let keys = req.params.keys
+    let accepts = req.accepts(['application/json', 'text/html'])
+    let context = sound_search(soundurl + keys + '&fields=name,description,previews' + '&token=' + apikey)
+        .then((context) => {
+            if (accepts === 'application/json') {
+                res.status(200).json(context);
+            }
+            else {
+                res.status(200);
+                res.render('results', {
+                    layout: 'index',
+                    context
+                });
+            }
+
+            console.log(context)
+            return context
+
+        })
+});
 
 
 module.exports = router;
